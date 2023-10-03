d998e9052acbbc242f549ff0a82a633ef9ec5066
abbyvondracek <145507225+abbyvondracek@users.noreply.github.com>
2023-10-03 17:42:52 -0500

    Update IntakeSubsystem.java

--- Paul/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ Paul/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -35,18 +35,6 @@ public class IntakeSubsystem extends SubsystemBase {
     this.frontRightMotor.set(-speed);
   }
 
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a
-   * digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
-
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
