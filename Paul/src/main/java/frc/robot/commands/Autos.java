877a4ce35a2fe2ee93990cee235b1e88d83d13d9
jmielke763 <jmielke763@gmail.com>
2023-11-14 11:40:52 -0600

    Update Autos.java

--- Paul/src/main/java/frc/robot/commands/Autos.java
+++ Paul/src/main/java/frc/robot/commands/Autos.java
@@ -8,11 +8,37 @@ import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 
-public final class Autos {
+public class Autos extends CommandBase {
   /** Example static factory for an autonomous command. */
   public static CommandBase exampleAuto(DriveTrain subsystem) {
     return Commands.sequence(subsystem.exampleMethodCommand());
   }
+
+  private DriveTrain drive; 
+  public Auto drive(DriveTrain drive){
+    this.drive = drive; 
+  }
+
+  private boolean timerBoolean(double startTime, double duration){
+    double delayTime = startTime + duration; 
+
+    if(System.currentTimeMillis() <= delayTime)
+    {
+      return true; 
+    } else {
+      return false; 
+    }
+  }
+
+
+  public void AutonomousForward(int time, double speed){
+    double delayTime = startTime + Dr
+
+     if(System.currentTimeMillis() <= delayTime)
+
+  }
+
+
 
   private Autos() {
     throw new UnsupportedOperationException("This is a utility class!");
