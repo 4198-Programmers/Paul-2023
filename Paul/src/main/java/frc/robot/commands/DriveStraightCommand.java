62020e01925b531926737882e906f03d479bdd20
41robocats98 <60160962+41robocats98@users.noreply.github.com>
2023-11-18 08:15:37 -0600

    save

--- Paul/src/main/java/frc/robot/commands/DriveStraightCommand.java
+++ Paul/src/main/java/frc/robot/commands/DriveStraightCommand.java
@@ -5,11 +5,38 @@ import frc.robot.subsystems.DriveTrain;
 
 public class DriveStraightCommand extends CommandBase{
   
-  public void driveStraightCommand(double speed, double distance){
-    while (distance >= currentDistancePosition()){
-      DriveTrain.drive(speed, speed);
-    } 
-    double resetPosition;
+  public DriveStraightCommand(DriveTrain drivetrain, double distanceInches){
+  this.driveTrain = drivetrain;
+  this.distanceInches = distanceInches;
+
   }
+  private DriveTrain driveTrain;
+  private 
+
+  public double currentDistancePosition(){
+    double currentDistancePosition = (Math.PI)(RelativeEncoder.frontLeftEncoder())(Constants.diameterOfWheel); 
+    return Math.round(currentDistancePosition);
+  }
+  
+  @Override
+  public void initialize() {}
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      this.intakeSubsystem.eject();
+    } 
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    this.intakeSubsystem.stop();
     
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
 }
