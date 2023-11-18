fb2e2193a8a00a639a1e0de134ada3a99eae00c2
41robocats98 <60160962+41robocats98@users.noreply.github.com>
2023-11-18 11:43:43 -0600

    Update EjectCommand.java

--- Paul/src/main/java/frc/robot/commands/EjectCommand.java
+++ Paul/src/main/java/frc/robot/commands/EjectCommand.java
@@ -5,14 +5,22 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.IntakeSubsystem;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class EjectCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final IntakeSubsystem intakeSubsystem;
+  private double timeToStartTopMotor;
+  private double timeToFinish;
 
   /**
+   //start bottom motor
+   //continously run bottom motor
+   //start top motor
+  //stop both motors
+
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
@@ -26,11 +34,18 @@ public class EjectCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    this.timeToStartTopMotor = Timer.getFPGATimestamp() + 2;
+    this.timeToFinish = this.timeToStartTopMotor + 1;
+  }
   // Called every time the scheduler runs while the command is scheduled.
+  
   @Override
   public void execute() {
-      this.intakeSubsystem.bottomIntake();
+      this.intakeSubsystem.bottomOuttake();
+    if(this.timeToStartTopMotor < Timer.getFPGATimestamp()){
+      this.intakeSubsystem.topOuttake();
+    }
     } 
 
   // Called once the command ends or is interrupted.
@@ -43,6 +58,6 @@ public class EjectCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return this.timeToFinish < Timer.getFPGATimestamp();
   }
 }
